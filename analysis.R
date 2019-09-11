51618d6860d7b18deafcd602ece8b494c564419b
Dirk Sarpe <dirk.sarpe@idiv.de>
2019-09-12 00:51:29 +0200

    readd accidentially removed age histogram

--- analysis.R
+++ analysis.R
@@ -23,6 +23,10 @@ plot_multicolumn <- function(variable, title) {
     ggtitle(title)
 }
 
+questionary %>%
+  ggplot(aes(x = Alter, fill = Geschlecht)) +
+  geom_histogram(bins = 10)
+
 # linux was released in 1991, questionary was 2019
 # adding a year for really early birds
 max_usage_years <- 2019 - 1991 + 1
@@ -30,7 +34,6 @@ questionary %>%
   filter(Dauer.der.Nutzung.von.Linux < max_usage_years) %>%
   ggplot(aes(x = Dauer.der.Nutzung.von.Linux, fill = Altersgruppe)) +
   geom_histogram(bins = 10)
-
 
 plot_multicolumn(Private.Nutzung, "Private Nutzung")
 plot_multicolumn(Berufliche.Nutzung, "Berufliche Nutzung")
