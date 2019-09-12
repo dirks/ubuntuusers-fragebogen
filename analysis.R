ad59d8f0bdf638c0918bc1319733a6bc557e115f
Dirk Sarpe <dirk.sarpe@idiv.de>
2019-09-12 08:29:31 +0200

    add frequency tables for questions qith one answer

--- analysis.R
+++ analysis.R
@@ -23,6 +23,15 @@ plot_multicolumn <- function(variable, title) {
     ggtitle(title)
 }
 
+frequency_table <- function(variable) {
+  variable <- enquo(variable)
+
+    questionary %>%
+    group_by(!! variable) %>%
+    tally() %>%
+    mutate(percent = round(100 * (n / sum(n)), 1))
+}
+
 questionary %>%
   ggplot(aes(x = Alter, fill = Geschlecht)) +
   geom_histogram(bins = 10)
@@ -35,6 +44,18 @@ questionary %>%
   ggplot(aes(x = Dauer.der.Nutzung.von.Linux, fill = Altersgruppe)) +
   geom_histogram(bins = 10)
 
+# one selection per question
+frequency_table(Geschlecht)
+frequency_table(Grund.für.Anmeldung)
+frequency_table(Worüber.erfolgte.der.Einstieg.bei.ubuntuusers.de.)
+frequency_table(Wie.bewertest.du.den.optischen.Auftritt.von.ubuntuusers.de.)
+frequency_table(Wie.bewertest.du.den.Funktionsumfang.und.die.Bedienung.der.Plattform.)
+frequency_table(Wie.bewertest.du.Entscheidungen.des.Teams..z..B..Moderationsentscheidungen.oder.Anforderungen.des.Wikiteams..)
+frequency_table(Wie.finanziert.sich.ubuntuusers.de.deiner.Meinung.nach.)
+frequency_table(Welche.Verbindung.hat.ubuntuusers.de.zu.Canonical.)
+frequency_table(Wer.ist.im.Team.von.ubuntuusers.de.)
+
+# multiple selections per question
 plot_multicolumn(Private.Nutzung, "Private Nutzung")
 plot_multicolumn(Berufliche.Nutzung, "Berufliche Nutzung")
 plot_multicolumn(Welche.Bereiche.von.ubuntuusers.de.nutzt.du.sehr.oft., "Welche Bereiche von ubuntuusers.de nutzt du sehr oft?")
